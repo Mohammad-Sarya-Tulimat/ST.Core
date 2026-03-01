9237750d9e1c1fab30327a65dd2a511d4aa26b57
Mohammad-Sarya-Tulimat <sarya.tulaimat@outlook.com>
2026-03-01 22:17:39 +0300

    add validation

---
+++ ST.Core/Contract/ISTValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ST.Core.Contract
+{
+    public interface ISTValidator<T>
+    {
+        ValidationResult Validate(T value);
+        ValidationResult ValidateAsync(T value, CancellationToken cancellationToken = default);
+    }
+}
---
+++ ST.Core/Models/ValidationResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ST.Core.Models
+{
+    public class ValidationResult
+    {
+        public virtual bool IsValid => Errors==null | Errors.Any();
+        public Dictionary<string, List<string>> Errors { get; set; } = new Dictionary<string, List<string>>();
+        public ValidationResult AddError(string propertyName, string errorMessage)
+        {
+            Errors = Errors ?? new Dictionary<string, List<string>>();
+            if (!Errors.ContainsKey(propertyName))
+                Errors[propertyName] = new List<string>();
+            Errors[propertyName].Add(errorMessage); 
+            return this;
+        }
+    }
+}
