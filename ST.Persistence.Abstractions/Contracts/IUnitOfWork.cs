94d90a8b2b8b053451334437b36073e4bbefa292
Mohammad-Sarya-Tulimat <sarya.tulaimat@outlook.com>
2026-08-30 20:35:47 +0300

    update ef core

--- ST.Persistence.Abstractions/Contracts/IUnitOfWork.cs
+++ ST.Persistence.Abstractions/Contracts/IUnitOfWork.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ST.Persistence.Abstractions.Contracts
 {
-    public interface IUnitOfWork<TRepo>: IDisposable where TRepo : class
+    public interface IUnitOfWork<out TRepo> : IDisposable where TRepo : class
     {
         TRepo Repositories { get; }
         int SaveChanges();
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken=default);
-        ITransaction GetTransaction(System.Data.IsolationLevel isolation= System.Data.IsolationLevel.ReadCommitted);
-        Task<ITransaction> GetTransactionAsync(System.Data.IsolationLevel isolation = System.Data.IsolationLevel.ReadCommitted,CancellationToken cancellationToken=default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        ITransaction GetTransaction(System.Data.IsolationLevel isolation = System.Data.IsolationLevel.ReadCommitted);
+        Task<ITransaction> GetTransactionAsync(System.Data.IsolationLevel isolation = System.Data.IsolationLevel.ReadCommitted, CancellationToken cancellationToken = default);
 
     }
 }
